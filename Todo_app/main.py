962fb70ac5060603fbe2cda2133b34d19a6592cb
Gokul Km <gokulkm@Gokuls-MacBook-Air.local>
2022-03-30 20:28:51 +0200

    added complete task

--- Todo_app/main.py
+++ Todo_app/main.py
@@ -1,20 +1,62 @@
 import os
+import sys
 import tkinter as tk
 print("please enter to start:")
+global comp_ref_list ,row, modi_once
+comp_ref_list = []
+global modi_once
+modi_once = 0
 while True:
-    def Todo_application(ss=""):
+    def Todo_application(in_val):
+
+        row =1
+        d=in_val.split()
+        if len(d)==0:
+            ss=""
+        if len(d) ==1:
+            ss=d[0]
+
+        if len(d) > 1:
+            ss = d[0]
+            row = d[1]
+
         if ss=="":
             myfile = open("data/initial_setup")
             for item in myfile:
                 print(item, end="")
             print("\n")
             print("Select an option")
+
             myfile.close()
+        if ss == ("-c"):
+            task = open("data/task")
+            if modi_once == 0:
+                j = 0
+                for i in task:
+                    j += 1
+                    comp_ref_list.append("")
+
+            task = open("data/task")
+            j = 1
+            for i in task:
+                if j == int(row):
+                    comp_ref_list[j-1]=str("X")
+                else :
+                    pass
+                print(j, "-", "["+str(comp_ref_list[j-1])+"]", i, end="")
+                j += 1
         if ss==("-l"):
             task = open("data/task")
-            j=1
+            if modi_once==0:
+                j = 0
+                for i in task:
+                    j += 1
+                    comp_ref_list.append("")
+
+            j = 0
+            task = open("data/task")
             for i in task:
-                print(j ,"-",i, end="")
+                print(j+1 ,"-","["+str(comp_ref_list[j])+"]",i, end="")
                 j+=1
             print("\n")
             print("Select an option or press enter to list commands")
@@ -23,26 +65,20 @@ while True:
             print("please enter the task :")
             new_task = str(input())
             task = open("data/task","a")
-            task.write(("\n"))
             task.write((new_task))
+            task.write(("\n"))
             task.close()
             print("Select an option or press enter to list commands")
         if ss == ("-r"):
             with open("data/delete", "w") as file:
                 pass
+            k=1
             task = open("data/task")
-            j = 1
-            for i in task:
-                print(j, "-", i, end="")
-                j += 1
-            print("\n please enter the number of task to delete :")
-            del_task = int(input())
-            k = 1
-            task = open("data/task")
+
             for i in task:
                 task = open("data/task","a")
                 dele_task = open("data/delete", "a")
-                if k==del_task:
+                if k==int(row):
                     print("deleted")
                     k += 1
                 else :
@@ -62,6 +98,8 @@ while True:
             task.close()
             os.remove("data/delete")
             print("Select an option or press enter to list commands")
+
+
 
 
     Todo_application(str(input()))
