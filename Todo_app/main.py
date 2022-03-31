2d8477384e46a1ec2c4d2e910b6324c720917134
Gokul Km <gokulkm@Gokuls-MacBook-Air.local>
2022-03-31 23:44:01 +0200

    -c added

--- Todo_app/main.py
+++ Todo_app/main.py
@@ -2,10 +2,17 @@ import os
 import sys
 import tkinter as tk
 print("please enter to start:")
+
+global compvar
+
 global comp_ref_list ,row, modi_once
-comp_ref_list = []
-global modi_once
 modi_once = 0
+
+comp_ref_list = []
+compvar=open("data/comp_stat")
+for item in compvar:
+    comp_ref_list.append(item)
+print(comp_ref_list)
 while True:
     def Todo_application(in_val):
 
@@ -41,10 +48,26 @@ while True:
             for i in task:
                 if j == int(row):
                     comp_ref_list[j-1]=str("X")
-                else :
-                    pass
-                print(j, "-", "["+str(comp_ref_list[j-1])+"]", i, end="")
+
+                print(j, "-", "["+comp_ref_list[j-1].strip("\n")+"]", i, end="")
                 j += 1
+            print(comp_ref_list)
+            item=0
+            os.remove("data/comp_stat")
+            task = open("data/task")
+            for ite in task:
+                    compvar = open("data/comp_stat","a")
+                    compvar.write(comp_ref_list[item]+"\n")
+                    item+=1
+        if ss == ("-u"):
+            comp_ref_list[int(int(row)-1)]=""
+            item = 0
+            os.remove("data/comp_stat")
+            task = open("data/task")
+            for ite in task:
+                compvar = open("data/comp_stat", "a")
+                compvar.write(comp_ref_list[item] + "\n")
+                item += 1
         if ss==("-l"):
             task = open("data/task")
             if modi_once==0:
@@ -56,16 +79,17 @@ while True:
             j = 0
             task = open("data/task")
             for i in task:
-                print(j+1 ,"-","["+str(comp_ref_list[j])+"]",i, end="")
+                print(j+1 ,"-","["+str(comp_ref_list[j].strip("\n"))+"]",i, end="")
                 j+=1
             print("\n")
             print("Select an option or press enter to list commands")
             task.close()
         if ss==("-a"):
-            print("please enter the task :")
-            new_task = str(input())
+            # print("please enter the task :")
+            new_task = row
+            print(new_task)
             task = open("data/task","a")
-            task.write((new_task))
+            task.write((str(new_task)))
             task.write(("\n"))
             task.close()
             print("Select an option or press enter to list commands")
@@ -84,8 +108,13 @@ while True:
                 else :
                     dele_task.write(i)
                     k += 1
-
                 task.close()
+            for item in range (int(row)-1 ,len(comp_ref_list)-1):
+
+                comp_ref_list[int(item)]=comp_ref_list[int(item)+1]
+
+            print(comp_ref_list)
+
             dele_task = open("data/delete")
             task = open("data/task", "a")
             os.remove("data/task")
@@ -100,9 +129,9 @@ while True:
             print("Select an option or press enter to list commands")
 
 
-
-
     Todo_application(str(input()))
+
+
 
 
 
